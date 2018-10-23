2521446dccfcec060f9f8f0f331dc3234912b2e5
Alexandro <tonytony9877@gmail.com>
2018-10-23 21:45:59 +0300

    add page authorization

--- app/src/main/java/ru/bur/cometogetherandroid/AuthorizationActivity.java
+++ app/src/main/java/ru/bur/cometogetherandroid/AuthorizationActivity.java
@@ -1,13 +1,15 @@
 package ru.bur.cometogetherandroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class SscrollerMeetings extends AppCompatActivity {
+public class AuthorizationActivity  extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sscroller_meetings);
+        setContentView(R.layout.authorization);
     }
+
 }
