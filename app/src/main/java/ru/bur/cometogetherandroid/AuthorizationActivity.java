4057619a65e2514fa63d5b5dfa22efec11ff2308
aburykin <aburykin@stream.com>
2018-11-05 11:45:42 +0300

    add menu

--- app/src/main/java/ru/bur/cometogetherandroid/AuthorizationActivity.java
+++ app/src/main/java/ru/bur/cometogetherandroid/AuthorizationActivity.java
@@ -3,6 +3,8 @@ package ru.bur.cometogetherandroid;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,8 +16,6 @@ import ru.bur.cometogetherandroid.task.AskServer;
 
 public class AuthorizationActivity extends AppCompatActivity {
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +25,7 @@ public class AuthorizationActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.editText);
         button.setOnClickListener(
                 (v) -> {
-                        new AskServer().execute(textView.getText().toString());
+                    new AskServer().execute(textView.getText().toString());
 //                    restTemplate.postForLocation(CONNECTION_URL, restTemplate.postForLocation(CONNECTION_URL, textView.getText()));
 //                    startActivity(new Intent(this, MeetingScrollerActivity.class));
                 }
@@ -38,4 +38,44 @@ public class AuthorizationActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        menu.add(1, 1, 1, "Авторизация");
+        menu.add(1, 2, 2, "Список встреч");
+        menu.add(1, 3, 3, "Создать новую встречу");
+        menu.add(1, 4, 4, "Посмотреть встречу");
+        menu.add(1, 5, 5, "Посмотреть участников встречи");
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent;
+        switch (item.getItemId()) {
+            case 1:
+                intent= new Intent(this, AuthorizationActivity.class);
+                startActivity(intent);
+                break;
+            case 2:
+                 intent = new Intent(this, MeetingScrollerActivity.class);
+                startActivity(intent);
+                break;
+            case 3:
+               //  intent = new Intent(this, .class);
+              //  startActivity(intent);
+                break;
+            case 4:
+             //    intent = new Intent(this, .class);
+              //  startActivity(intent);
+                break;
+            case 5:
+                 intent = new Intent(this, ParticipantsActivity.class);
+                startActivity(intent);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
