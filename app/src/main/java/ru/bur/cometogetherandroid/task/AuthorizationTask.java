e1514241d3b8a1c8a4d2f394e0e04c126ffefd11
aburykin <aburykin@stream.com>
2018-11-21 19:18:24 +0300

    add api

--- app/src/main/java/ru/bur/cometogetherandroid/task/AuthorizationTask.java
+++ app/src/main/java/ru/bur/cometogetherandroid/task/AuthorizationTask.java
@@ -6,20 +6,44 @@ import android.util.Log;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
+import ru.bur.cometogetherandroid.util.AppClient;
+import ru.bur.dto.AppUserDto;
+import ru.bur.dto.AuthDto;
+
 public class AuthorizationTask extends AsyncTask<String, Void, String> {
     private String LOG_TAG = "cometogetherandroid ";
 
-
-    private final String CONNECTION_URL = "http://10.0.2.2:8080/rest/android";
-
     @Override
     protected String doInBackground(String... strings) {
+
+
+
+
+      //  AuthDto authDto = new AuthDto();
+      //  authDto.setPhoneNumber(strings[0]);
+      //  Log.d("LOG_TAG", "authDto="+authDto);
+
         RestTemplate restTemplate = new RestTemplate();
-        String phoneNumber = strings[0];
-        Log.d("LOG_TAG", "phoneNumber="+phoneNumber);
         restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
-        String result = restTemplate.postForObject(CONNECTION_URL, phoneNumber, String.class);
-        return result;
+
+      //  String test_rest = "{\"phoneNumber\": \"7771243\",\"authorizationToken\": \"2\"}";
+
+       // String result = restTemplate.postForObject(AppClient.getUrlByLabel("appUser"), test_rest ,String.class);
+       /* String result = restTemplate.postForObject(
+                "http://10.0.2.2:8080/profile/appUsers",
+                "{\"phoneNumber\": \"\",\"authorizationToken\": \"\",\"firstName\": \"\",\"lastName\": \"123\",\"birthdayDate\": null,\"age\": 0}"
+                ,String.class);*/
+        AppUserDto appUserDto = new AppUserDto();
+        appUserDto.setPhoneNumber("1231231239");
+
+        // String result = restTemplate.postForObject(AppClient.getUrlByLabel("appUser"), appUserDto ,String.class);
+       // String result = restTemplate.postForObject(AppClient.getUrlByLabel("appUser"), "123" ,String.class);
+        String result = restTemplate.postForObject("http://10.0.2.2:8080/rest/android", "123" ,String.class);
+
+        System.out.println(result);
+        Log.d("LOG_TAG", "result="+result);
+
+        return "XXX";
     }
 
 }
---
+++ app/src/main/java/ru/bur/cometogetherandroid/util/AppClient.java
@@ -0,0 +1,34 @@
+package ru.bur.cometogetherandroid.util;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *  Получает общий список запросов. И посылает их
+ *
+ */
+public class AppClient {
+
+    static String baseUrl ="http://10.0.2.2:8080/profile/";
+
+
+    static Map<String,String> AllServerLables = new HashMap<>(); // label
+    static {
+        AllServerLables.put("meetings","meetings");
+        AllServerLables.put("appUsers","appUsers");
+    }
+
+  /*  public void getAllUrls(){
+
+
+    }*/
+
+  public static String getUrlByLabel(String label){
+      return baseUrl+label;
+  }
+
+
+}
